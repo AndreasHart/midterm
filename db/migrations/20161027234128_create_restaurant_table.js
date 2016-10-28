c1ac5ae00bf1405419a62942a3a718d91477300e
Andreas Hart <andreastheohart@gmail.com>
2016-10-28 17:14:06 +0000

    initial database structure

--- db/migrations/20161027234128_create_restaurant_table.js
+++ db/migrations/20161027234128_create_restaurant_table.js
@@ -1,21 +1,22 @@
+
 exports.up = function(knex, Promise) {
   return  Promise.all([
-    knex.schema.createTable('users', function (table) {
-      table.increments();
-      table.string('name');
-      table.string('phoneNumber');
-      table.string('address')
+    knex.schema.createTable('restaurants', function (table) {
+      table.increments('id');
+      table.string('restaurant_name')
+      table.string('description')
+      table.boolean('delivery')
+      table.boolean('pickup')
       table.boolean('glutenFree')
       table.boolean('dairyFree')
       table.boolean('vegetarian')
       table.boolean('vegan')
-      table.string('allergies')
     })
   ])
 };
 
 exports.down = function(knex, Promise) {
   return  Promise.all([
-    knex.schema.dropTable('users')
-  ])
+    knex.schema.dropTable('restaurants')
+    ])
 };
---
+++ db/migrations/20161028160332_connect_orders_orderItems.js
@@ -0,0 +1,8 @@
+//in the next migration I added the column needed for connecting orders and orderitem
+exports.up = function(knex, Promise) {
+
+};
+
+exports.down = function(knex, Promise) {
+
+};
