91fb50be891920bf31106f515af54a93562a92ef
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-10-30 17:22:44 +0000

    major restructuring of the restaurant page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,43 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });
+"Use Strict"
+
+$(document).ready(function () {
+
+	$('.col-xs-12').mouseenter(function () {
+		$(this).css("background-color", "darkseagreen");
+	});
+
+	$('.col-xs-12').mouseleave(function () {
+		$(this).css("background-color", "lightseagreen");
+	});
+
+	$('.lunch-food-description').hide();
+	$('.lunch').mouseenter(function () {
+		$('.lunch-food-description').slideToggle();
+	});
+
+	$('.lunch-article').mouseleave(function () {
+		$('.lunch-food-description').hide();
+	});
+
+
+	var createRestElem = (loadRestaurants) => {
+		var $restaurant = ``;
+		return $restaurant;
+	}
+
+
+	// $(() => {
+	//   $.ajax({
+	//     method: "GET",
+	//     url: "/api/users"
+	//   }).done((users) => {
+	//     for(user of users) {
+	//       $("<div>").text(user.name).appendTo($("body"));
+	//     }
+	//   });
+
+	// });
+
+	//App should be able to populate 
 
 });
-
-
